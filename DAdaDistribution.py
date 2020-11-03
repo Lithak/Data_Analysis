3143b153862b11772502ab16edcb53126ee84475
Litha Kane <71072996+Lithak@users.noreply.github.com>
2020-11-03 11:40:39 +0200

    Add files via upload
    data distribution

---
+++ DAdaDistribution.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+#Data Analysis
+import matplotlib.pyplot as plt
+
+test_scores = [12, 99, 65, 85, 42]
+test_names = ["Andy", "Martin", "Zahara", "Vuyo", "Ziyaad"]
+x_pos = [i for i, _ in enumerate(test_names)]
+#labels on the x-axis
+#labeling and visuals
+plt.bar(x_pos, test_scores, color='blue')
+plt.xlabel("Names")
+plt.ylabel("Marks(%")
+plt.xticks(x_pos, test_names)
+plt.show()
